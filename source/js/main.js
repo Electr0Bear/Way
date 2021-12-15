76c94770ed7ca26aba1f3ba36d83939d6884d08d
Electr0Bear <dmitry.kn@protonmail.com>
2021-12-15 12:52:14 +0300

    стилизует по ПП

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,9 @@
 
 import { sliderHandler } from './slider.js';
 import { modalHandler } from './modal.js';
+import { menuHandler, onLoadJs } from './header-menu.js'
 
+onLoadJs();
 sliderHandler();
 modalHandler();
+menuHandler();
--- source/js/modal.js
+++ source/js/modal.js
@@ -9,17 +9,31 @@ var modalHandler = function () {
     button.addEventListener('click', function (evt) {
       evt.preventDefault();
       if (!modal.classList.contains('modal--show')) {
-        modal.classList.toggle('modal--show');
+        modal.classList.add('modal--show');
+        closeModal();
       }
     });
+  });
+};
+
+var closeModal = function () {
+  document.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Escape') {
+      evt.preventDefault();
+      modal.classList.remove('modal--show');
+      document.removeEventListener('keydown', evt);
+    }
   });
 
   closeModalBtn.addEventListener('click', function (evt) {
     evt.preventDefault();
     if (modal.classList.contains('modal--show')) {
-      modal.classList.toggle('modal--show');
+      modal.classList.remove('modal--show');
+      closeModalBtn.removeEventListener('click', evt);
     }
   });
-};
+}
 
-export {modalHandler};
+export {
+  modalHandler
+};
