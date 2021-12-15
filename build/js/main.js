5b6323755c4a1142fda3985f0ff7733aaa4e0331
Electr0Bear <dmitry.kn@protonmail.com>
2021-12-15 21:54:17 +0300

    правит отступы в формах

--- build/js/main.js
+++ build/js/main.js
@@ -3,8 +3,11 @@
 import { sliderHandler } from './slider.js';
 import { modalHandler } from './modal.js';
 import { menuHandler, onLoadJs } from './header-menu.js'
+import { onSubmitForm, checkFormValidity } from './form.js'
 
 onLoadJs();
 sliderHandler();
 modalHandler();
 menuHandler();
+checkFormValidity();
+onSubmitForm();
--- source/js/main.js
+++ source/js/main.js
@@ -3,10 +3,11 @@
 import { sliderHandler } from './slider.js';
 import { modalHandler } from './modal.js';
 import { menuHandler, onLoadJs } from './header-menu.js'
-import { onSubmitForm } from './form.js'
+import { onSubmitForm, checkFormValidity } from './form.js'
 
 onLoadJs();
 sliderHandler();
 modalHandler();
 menuHandler();
+checkFormValidity();
 onSubmitForm();
